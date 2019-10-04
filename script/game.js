134136ee8ef2a8c2c714ff147838f52f745f0eca
ionpascaru <iionpascaru@gmail.com>
2019-10-04 22:41:25 +0200

    2 & 3 - draw ships in fixed position

--- script/game.js
+++ script/game.js
@@ -1,19 +1,57 @@
 // grid rows + columns + size of square
-var rows = 10;
-var cols = 10;
-var squareSize = 50;
+let rows = 10;
+let cols = 10;
+let squareSize = 50;
 
 //container element
-var gameBoardContainer = document.getElementById("gameboard");
+let gameBoardContainer = document.getElementById("gameboard");
 
 for (i = 0; i < cols; i++) {
-  for (j = 0; j < rows; j++){
-    var square = document.createElement("div");
+  for (j = 0; j < rows; j++) {
+    let square = document.createElement("div");
     gameBoardContainer.appendChild(square);
-    square.id = "s" + j +i;
-    var topPosition = j * squareSize;
-    var leftPosition = i * squareSize;
+    square.id = "s" + j + i;
+    let topPosition = j * squareSize;
+    let leftPosition = i * squareSize;
     square.style.top = topPosition + "px";
     square.style.left = leftPosition + "px";
   }
 }
+
+//array with status of square:
+//0 = emty, 1 = ship part, 2 = sunken ship part, 3 = missed shot
+
+let gameBoard = [
+  [0,0,1,1,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,1,1,0,0]
+  ];
+
+  generateBattleships();
+
+  function generateBattleships(){
+    return gameBoard;
+  }
+  
+  show();
+  
+  function show(e) {
+    for (i = 0; i < cols; i++) {
+      for (j = 0; j < rows; j++) {
+        //find ship and make it red
+        if (
+          gameBoard[i][j] == 1 &&
+          document.getElementById("s" + i + j).style.background != "red"
+        ) {
+          document.getElementById("s" + i + j).style.background = "red";
+        }
+      }
+    }
+  }
